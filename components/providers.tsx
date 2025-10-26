46126b756d787d6cd612763205cee815955eb67b
Amar <ambir0513@gmail.com>
2025-10-27 00:25:47 +0530

    config(init): nextjs, shadcn, tailwindcss

---
+++ components/providers.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { ThemeProvider } from "next-themes";
+import React from "react";
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider
+      enableSystem
+      disableTransitionOnChange
+      enableColorScheme
+      storageKey="theme"
+      defaultTheme="system"
+      attribute="class"
+    >
+      {children}
+    </ThemeProvider>
+  );
+};
---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "unused-imports/no-unused-vars": [
+        "warn",
+        { vars: "all", varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
+      ],
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            ["builtin", "external"],
+            ["internal"],
+            ["parent", "sibling", "index"],
+          ],
+        },
+      ],
+    },
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+    ],
+  },
+];
+
+export default eslintConfig;
